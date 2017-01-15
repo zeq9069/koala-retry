d767017cbfb0e5d20dc12c0ac5f3f86ade4438bb
erqiang.zhang <erqiang.zhang@baichanghui.com>
2017-01-15 13:25:55 +0800

    update

--- src/main/java/com/kyrincloud/koala_retry/interceptors/RetryInterceptor.java
+++ src/main/java/com/kyrincloud/koala_retry/interceptors/RetryInterceptor.java
@@ -2,7 +2,6 @@ package com.kyrincloud.koala_retry.interceptors;
 
 import java.lang.reflect.Method;
 
-import com.kyrincloud.koala_retry.annotation.Retry;
 import com.kyrincloud.koala_retry.policy.SimpleRetryPolicy;
 import com.kyrincloud.koala_retry.support.RetryContext;
 
@@ -31,33 +30,29 @@ public class RetryInterceptor {
 	
 	public Object invoke() throws Throwable {
 		Object result = null;
-				if(method.isAnnotationPresent(Retry.class)){
-					int count = 0;
-					SimpleRetryPolicy policy = context.getPolicy(method); 
-					Throwable lastExeception = null;
-					try{
-						count++;
-						result = proxy.invokeSuper(obj, args);
-					}catch(Throwable e){
-						lastExeception = e;
-					}
-					while(policy.canRetry(count,lastExeception) && !policy.isLast(count,lastExeception)){
-							try{
-								policy.getSleep().sleep();
-								result = proxy.invokeSuper(obj, args);
-							}catch(Throwable e){
-								lastExeception = e;
-							}
-						}
-					if(policy.isLast(count,lastExeception)){
-						policy.getSleep().sleep();
-						result = proxy.invokeSuper(obj, args);
-					}else if(lastExeception != null){
-						throw lastExeception;
-					}
-				}else{
-					result = proxy.invokeSuper(obj, args);
-				}
+		int count = 0;
+		SimpleRetryPolicy policy = context.getPolicy(method); 
+		Throwable lastExeception = null;
+		try{
+			count++;
+			result = proxy.invokeSuper(obj, args);
+		}catch(Throwable e){
+			lastExeception = e;
+		}
+		while(policy.canRetry(count,lastExeception) && !policy.isLast(count,lastExeception)){
+			try{
+				policy.getSleep().sleep();
+				result = proxy.invokeSuper(obj, args);
+			}catch(Throwable e){
+				lastExeception = e;
+			}
+		}
+		if(policy.isLast(count,lastExeception)){
+			policy.getSleep().sleep();
+			result = proxy.invokeSuper(obj, args);
+		}else if(lastExeception != null){
+			throw lastExeception;
+		}
 		return result;
 	}
 
